ba2ae6f479ee2b25c5355c252a6a1c6a8959c461
Florian Fröhlich <44257732+ffsws@users.noreply.github.com>
2020-08-07 20:50:14 +0200

    Create quota.js

---
+++ www/quota.js
@@ -0,0 +1,13 @@
+// Set custom notification messages per quota type/key
+window.OPENSHIFT_CONSTANTS.QUOTA_NOTIFICATION_MESSAGE = {
+  'pods': 'Die Anzahl der Pods wurde überschritten! Kontaktieren Sie <a href="mailto:container@sws.de">Container Team</a> um weitere Ressourcen zu buchen.',
+  'limits.memory': 'Das Arbeitsspeicher-Limit wurde überschritten! Kontaktieren Sie <a href="mailto:container@sws.de">Container Team</a> um weitere Ressourcen zu buchen.'
+  'cpu': 'Die Anzahl der CPUs wurde überschritten! Kontaktieren Sie <a href="mailto:container@sws.de">Container Team</a> um weitere Ressourcen zu buchen.',
+  'limits.cpu': 'Das CPU-Limit wurde überschritten! Kontaktieren Sie <a href="mailto:container@sws.de">Container Team</a> um weitere Ressourcen zu buchen.'
+  'limits.ephemeral-storage': 'Das Speicher-Limit für flüchtigen Speicher wurde überschritten! Kontaktieren Sie <a href="mailto:container@sws.de">Container Team</a> um weitere Ressourcen zu buchen.',
+  'memory': 'Das Arbeitsspeicher-Limit wurde überschritten! Kontaktieren Sie <a href="mailto:container@sws.de">Container Team</a> um weitere Ressourcen zu buchen.'
+  'ephemeral-storage': 'Die Anzahl der Pods wurde überschritten! Kontaktieren Sie <a href="mailto:container@sws.de">Container Team</a> um weitere Ressourcen zu buchen.',
+  'requests.cpu': 'Das CPU-Limit wurde überschritten! Kontaktieren Sie <a href="mailto:container@sws.de">Container Team</a> um weitere Ressourcen zu buchen.'
+  'requests.memory': 'Das Arbeitsspeicher-Limit wurde überschritten! Kontaktieren Sie <a href="mailto:container@sws.de">Container Team</a> um weitere Ressourcen zu buchen.',
+  'requests.ephemeral-storage': 'Das Speicher-Limit für flüchtigen Speicher wurde überschritten! Kontaktieren Sie <a href="mailto:container@sws.de">Container Team</a> um weitere Ressourcen zu buchen.'
+};
